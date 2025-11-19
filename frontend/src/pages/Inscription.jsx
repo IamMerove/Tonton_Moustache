3df43fe0ffd32a46017db8edff0d95deba6c1c1f
os974 <olivier.schollaert@live.be>
2025-11-19 11:54:17 +0400

    Style inscription modifié

--- frontend/src/pages/Inscription.jsx
+++ frontend/src/pages/Inscription.jsx
@@ -1,13 +1,12 @@
-import Formulaire from "../templates/Form_inscription"; 
+import Formulaire from "../templates/Form_inscription";
 
-
-function Inscription_Page(params) {
-    return(
-        <>
-            <p>Ceci est la page d'inscriptionn</p>
+function Inscription_Page() {
+    return (
+        <div className="inscription-center-container">
+            <h2 className="inscription-title-gaming">Inscrivez-vous</h2>
             <Formulaire />
-        </>
-    )
+        </div>
+    );
 }
 
 export default Inscription_Page;
